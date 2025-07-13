74d77c86cf520f4ef0aec2e3ca72cfb0a6a4bbbf
Moa22men <moamenabdelrahman22@gmail.com>
2025-07-13 10:37:08 +0300

    Q1 solved

--- OOP Session 03/Program.cs
+++ OOP Session 03/Program.cs
@@ -2,11 +2,34 @@
 
 namespace OOP_Session_03
 {
+    #region Part 1 Q1
+    class Calculator
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int Add(int a, int b, int c)
+        {
+            return a + b + c;
+        }
+
+        public double Add(double a, double b)
+        {
+            return a + b;
+        }
+    }
+    #endregion
+
+    
+
+
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-        }
+            
+    }
     }
 }
